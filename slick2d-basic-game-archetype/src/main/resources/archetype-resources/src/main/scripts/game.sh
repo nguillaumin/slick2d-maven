21e3d37f5f2d6765b7ee895a39b55ea6a79c20d7
Stefan Hendriks <stefanhen83@gmail.com>
2015-01-22 10:06:24 +0100

    Remove trailing & so that when you run the game and quit it - it will not 'wait' for input but really quit.

--- slick2d-basic-game-archetype/src/main/resources/archetype-resources/src/main/scripts/game.sh
+++ slick2d-basic-game-archetype/src/main/resources/archetype-resources/src/main/scripts/game.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 BASE=`dirname $0`
-java -jar -Djava.library.path="$BASE/lib/" "$BASE/${project.build.finalName}.${project.packaging}" &
+java -jar -Djava.library.path="$BASE/lib/" "$BASE/${project.build.finalName}.${project.packaging}"
